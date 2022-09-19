e3d55e84ff916546d6f4021aa3e7209e61daf029
Aravind Gireesh Kumar - STUDENT <L00170985@atu.ie>
2022-09-19 11:19:53 +0100

    Pushed the contents of Document & Code Folders to the GitHub remote Rep

---
+++ Code_Folder/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+ public static void main(String[] args) {
+
+ System.out.println("Hello, LYIT from Aravind"); // Prints "Hello, LYIT from Aravind" as the output
+
+ }
+
+}
